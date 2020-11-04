88fcc5290a32e0ee4e316daabf2173ad4863a95c
unknown <ma.santanat@alumnos.duoc.cl>
2020-11-04 13:27:36 -0300

    Tienda Harkov: Creando Pagina de Inicio

--- TiendaHarkov/Juegos/urls.py
+++ TiendaHarkov/Juegos/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('steam/', views.steam, name='steam'),
+    path('origin', views.origin, name='origin'),
+    path('wallet2', views.wallet2, name='wallet2'),
+    path('preguntas', views.preguntas, name='preguntas'),
+    path('contacto', views.contacto, name='contacto'),
+    path('red', views.red, name='red'),
+    path('fallguys', views.fallguys, name='fallguys')
 ]
